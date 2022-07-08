f6254bf0a96c2979af1ddca7df9fdf384d9e5463
EthanChenYen-Peng <ypcethan@gmail.com>
2022-07-08 15:32:30 +1000

    adding styled components global style

---
+++ src/config/globalstyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+* {
+  margin: 0;
+}
+html, body {
+  height: 100%;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+`
+
+export default GlobalStyle
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
-import '../../styles/globals.css'
+// import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import theme from '@/config/theme'
+import GlobalStyle from '@/config/globalstyles'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
