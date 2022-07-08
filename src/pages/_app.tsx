ad81a34fbb33e8a9efa94d866bcbe5c03b5a00ca
EthanChenYen-Peng <ypcethan@gmail.com>
2022-07-09 09:02:47 +1000

    setup react query
    References:
    * https://prateeksurana.me/blog/mastering-data-fetching-with-react-query-and-next-js/

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,21 @@
-// import '../../styles/globals.css'
+import { useState } from 'react'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { ThemeProvider } from 'styled-components'
 import theme from '@/config/theme'
 import GlobalStyle from '@/config/globalstyles'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [client] = useState(() => new QueryClient())
   return (
     <>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
+        <QueryClientProvider client={client}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+          <ReactQueryDevtools />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   )
