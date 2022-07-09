3266ab52bf8060b9804bd6256288acff89abcfc2
EthanChenYen-Peng <ypcethan@gmail.com>
2022-07-09 15:04:44 +1000

    redirect user to dashboard page if they have already logged in.

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,13 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 const protectedRoutes = ['/dashboard']
+const publicRoutes = ['/', '/login', '/register']
 
 export default function middleware(req: NextRequest) {
+  const token = req.cookies.get('SHOWWCASE_ACCESS_TOKEN')
   if (protectedRoutes.find((p) => p === req.nextUrl.pathname)) {
-    const token = req.cookies.get('SHOWWCASE_ACCESS_TOKEN')
-
     if (!token) {
       return NextResponse.redirect(new URL('/login', req.url))
+    }
+  }
+
+  if (publicRoutes.find((p) => p === req.nextUrl.pathname)) {
+    if (token) {
+      return NextResponse.redirect(new URL('/dashboard', req.url))
     }
   }
 }
