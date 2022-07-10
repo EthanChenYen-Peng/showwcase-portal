d65f98460ddaade8d0c89c8a739a73df568c2af8
EthanChenYen-Peng <ypcethan@gmail.com>
2022-07-10 15:47:14 +1000

    adding Modal component.
    * Install `react-modal`

---
+++ src/components/utils/Modal.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactModal from 'react-modal'
+
+ReactModal.setAppElement('#__next')
+
+const customStyles = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(140, 140, 140, 0.5)',
+  },
+  content: {
+    width: '70%',
+    height: '70%',
+    margin: 'auto auto',
+  },
+}
+
+interface Props {
+  children: React.ReactNode
+  isOpen: boolean
+  close: () => void
+}
+
+function Modal({ children, isOpen, close }: Props) {
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={close}
+      style={customStyles}
+      contentLabel="Example Modal"
+    >
+      {children}
+    </ReactModal>
+  )
+}
+
+export default Modal
--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,14 +1,20 @@
+import { useState } from 'react'
 import { GetServerSideProps } from 'next'
 import { getUserFromToken } from '@/utils/auth'
 import type { IUser } from '@/lib/types'
 import MainLayout from '@/components/layout/MainLayout'
 import { Text, Stack, Box, Grid, Button } from '@/components/primitives'
+import Modal from '@/components/utils/Modal'
 
 interface Props {
   user: IUser
 }
+
 function Education({ user }: Props) {
+  const [modalIsOpen, setIsOpen] = useState(false)
   const { name } = user
+  const openModal = () => setIsOpen(true)
+  const closeModal = () => setIsOpen(false)
   return (
     <MainLayout user={user}>
       <Box width="90%" margin="0 auto">
@@ -16,9 +22,25 @@ function Education({ user }: Props) {
           <Text as="h2" textAlign="center" fontSize="1.5rem">
             Welcome to {name}&apos;s education page
           </Text>
-          <Button variant="primary" padding="1rem" fontSize="1.1rem">
+          <Button
+            variant="primary"
+            padding="1rem"
+            fontSize="1.1rem"
+            onClick={openModal}
+          >
             Add new education
           </Button>
+          <Modal isOpen={modalIsOpen} close={closeModal}>
+            <button onClick={closeModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+              <input />
+              <button>tab navigation</button>
+              <button>stays</button>
+              <button>inside</button>
+              <button>the modal</button>
+            </form>
+          </Modal>
         </Stack>
         <Grid gridTemplateColumns="repeat(12, 1fr)" gridGap="2rem">
           <Box gridColumn="span 3" bg="lightgray">
