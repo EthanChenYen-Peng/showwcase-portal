99fb2d84d071fb36939c87de206669582151dd06
EthanChenYen-Peng <ypcethan@gmail.com>
2022-07-11 20:28:30 +1000

    adding debounce search functionality for schools.
    * Install loadash for debounce utility function.

--- src/components/education/SchoolSelect.tsx
+++ src/components/education/SchoolSelect.tsx
@@ -1,5 +1,6 @@
 import AsyncSelect from 'react-select/async'
 import { fetchSchools } from '@/lib/api'
+import { debounce } from 'lodash'
 interface ISchool {
   name: string
 }
@@ -7,6 +8,27 @@ interface Props {
   school: string
   setSchool: (name: string) => void
 }
+
+interface SchoolOption {
+  value: string
+  label: string
+}
+
+function fetchSchoolsWithcalls(
+  inputText: string,
+  callback: (data: SchoolOption[]) => void
+) {
+  fetchSchools<{ schools: ISchool[] }>(inputText).then((response) => {
+    const data = response.schools.map((school) => {
+      return {
+        value: school.name,
+        label: school.name,
+      }
+    })
+    callback(data)
+  })
+}
+const debouncedFetchSchoolWithCallback = debounce(fetchSchoolsWithcalls, 500)
 
 function SchoolSelect({ setSchool }: Props) {
   return (
@@ -18,15 +40,7 @@ function SchoolSelect({ setSchool }: Props) {
         }
       }}
       loadOptions={(inputText, callback) => {
-        fetchSchools<{ schools: ISchool[] }>(inputText).then((response) => {
-          const data = response.schools.map((school) => {
-            return {
-              value: school.name,
-              label: school.name,
-            }
-          })
-          callback(data)
-        })
+        debouncedFetchSchoolWithCallback(inputText, callback)
       }}
     />
   )
